0f698eae14459e09e36cc1d0759d47c139a862aa
Enesssk <140720772+Enesssk@users.noreply.github.com>
2024-12-02 23:34:38 +0300

    feat(): completed fragment_entrance.xml

---
+++ app/src/main/java/com/example/weather_app_kotlin_mvvm_repository/ui/view/EntranceFragment.kt
@@ -0,0 +1,34 @@
+package com.example.weather_app_kotlin_mvvm_repository.ui.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.weather_app_kotlin_mvvm_repository.R
+import com.example.weather_app_kotlin_mvvm_repository.databinding.FragmentEntranceBinding
+
+
+class EntranceFragment : Fragment() {
+
+    private lateinit var binding : FragmentEntranceBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentEntranceBinding.inflate(inflater,container,false)
+
+
+
+        return binding.root
+    }
+
+
+}
