c36c05ebd90a08f9994934476eb0902cf0dff03c
Enesssk <140720772+Enesssk@users.noreply.github.com>
2024-12-03 21:04:58 +0300

    feat(): completed fragment_main.xml

---
+++ app/src/main/java/com/example/weather_app_kotlin_mvvm_repository/ui/view/MainFragment.kt
@@ -0,0 +1,35 @@
+package com.example.weather_app_kotlin_mvvm_repository.ui.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.weather_app_kotlin_mvvm_repository.R
+import com.example.weather_app_kotlin_mvvm_repository.databinding.FragmentMainBinding
+
+
+class MainFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentMainBinding.inflate(inflater,container,false)
+
+
+
+
+        return binding.root
+    }
+
+
+}
