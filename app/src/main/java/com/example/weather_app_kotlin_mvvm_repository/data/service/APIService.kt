d47fb380aee4470a1284ac42b359310054762b76
Enesssk <140720772+Enesssk@users.noreply.github.com>
2024-12-09 00:08:04 +0300

    feat(service): Create APIService.kt

--- app/src/main/java/com/example/weather_app_kotlin_mvvm_repository/data/service/APIService.kt
+++ app/src/main/java/com/example/weather_app_kotlin_mvvm_repository/data/service/APIService.kt
@@ -1,16 +1,17 @@
 package com.example.weather_app_kotlin_mvvm_repository.data.service
 
+import com.example.weather_app_kotlin_mvvm_repository.data.model.WeatherModel
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface APIService {
 
 
-    @GET("/data/2.5/weather")
+    @GET("data/2.5/weather?appid=937871ce6e334e836aaa378e7e26b614")
     suspend fun searchWeather(
-        @Query("q") cityName : String,
-        @Query("appid") apiKey : String
-    )
+        @Query("q") cityName : String
+    ) : Response<WeatherModel>
 
 
 }
